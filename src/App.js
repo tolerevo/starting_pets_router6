9be7501501f93c3e595b572eb1cc05f35d626aae
tolerevo <tolerevo@hotmail.com>
2024-02-02 22:02:27 -0600

    add Outlet route

--- src/App.js
+++ src/App.js
@@ -14,7 +14,11 @@ import {
 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Root />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/" element={<HomePage />} />
+    </Route>
+  )
 );
 
 function App() {
--- src/components/root/index.js
+++ src/components/root/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navigation from '../navigation';
+import { Outlet } from 'react-router-dom';
 // import Outlet
 
 const Root = () => {
     return (
         <>
             <Navigation/>
-            {/* Add an Outlet*/}
+            <Outlet />
         </>
     );
 };
